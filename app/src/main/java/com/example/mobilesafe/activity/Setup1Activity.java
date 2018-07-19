9dfb020fe0ebb1053204db86317de7c18ac1eb4e
Joe <719078007@qq.com>
2018-07-19 11:02:38 +0800

    add the project to vcs

--- app/src/main/java/com/example/mobilesafe/activity/Setup1Activity.java
+++ app/src/main/java/com/example/mobilesafe/activity/Setup1Activity.java
@@ -2,8 +2,11 @@ package com.example.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.mobilesafe.R;
+
+import static android.content.ContentValues.TAG;
 
 public class Setup1Activity extends Activity {
 
@@ -11,5 +14,7 @@ public class Setup1Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup1);
+		Log.i(TAG, "onCreate: 测试一下Git的使用");
+
 	}
 }
